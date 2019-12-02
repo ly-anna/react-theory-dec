6cdc1dcc2548dc2e95c8d16d1a9b604f04b56329
annsysadm <annsysadm@mail.ru>
2019-12-02 20:51:06 +0300

    added comments about props

--- jsprofi/src/Car/Car.js
+++ jsprofi/src/Car/Car.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default (props) => (
+// Это шаблон компонента - функция
+// данная функция может принимать в себя некоторые параметры
+// мы в него передаем свойства (props)
+// props.name - параметр props и уданного объекта есть поле name 
+
+export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong> {props.year} </strong></p>
