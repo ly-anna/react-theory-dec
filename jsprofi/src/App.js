62c8d9c1cdef9d730f993476918a469740d8f02f
annsysadm <annsysadm@mail.ru>
2019-12-02 21:13:40 +0300

    added comments about props2

--- jsprofi/src/App.js
+++ jsprofi/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Car from './Car/Car'
 
+
 export default 
 
 class App extends Component {
@@ -8,13 +9,22 @@ class App extends Component {
         const divStyle = {
             textAlign: 'center'
         }
+// используем фигурные скобки, когда передаем какой-то объект. 
+// name и year -  это и есть свойства(props) компонента Car - 
+// передаем свойства как значения атрибутов 
 
        return (
            <div style={divStyle}>
                <h1>Hello world!</h1>
 
-               <Car name={'Ford'} year={2018} />
-               <Car name={'Audi'} year={2016} />
+               <Car name={'Ford'} year={2018}>
+               <p style={{color: 'Blue'}}>COLOR</p>
+               </Car>
+               {/* меняем самозакрывающийся тег на  */}
+               <Car name={'Audi'} year={2016}>
+                   <p style={{color: 'red'}}>COLOR</p>
+               </Car>
+            
                <Car name={'Mazda'} year={2010} />
 
            </div>
